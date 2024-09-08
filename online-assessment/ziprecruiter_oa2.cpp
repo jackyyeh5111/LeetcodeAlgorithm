068f18bdb6895b4c11488bf51068f5f805bba125
Jacky2 <jackyyeh511@gmail.com>
2024-09-07 21:28:02 -0500

    backup

--- online-assessment/ziprecruiter_oa2.cpp
+++ online-assessment/ziprecruiter_oa2.cpp
@@ -1,63 +1,70 @@
-/* 
-  problem link: https://drive.google.com/file/d/1DGt8lFVaN8tV8_TwAvnSb_1Y8nhmtk5x/view?usp=drive_link
+/*
+  problem link:
+  https://drive.google.com/file/d/1DGt8lFVaN8tV8_TwAvnSb_1Y8nhmtk5x/view?usp=drive_link
  */
 #include "../utils.hpp"
 
 class Solution {
  public:
-  vector<int> output(vector<int> &times, int num_max_waiting = 1, int waiting_time = 300) {
-    int ptr_finished = 0;
-    int queue_size = 0;
+  vector<int> output(vector<int> &times, int num_max_waiting = 1,
+                     int waiting_time = 300) {
     int last_finish_time = 0;
-    vector<int> output(times.size());
+    int ptr_processing = 0;
+    int queue_size = 0;
+    vector<int> finished_times(times.size(), 0);
+
     for (int i = 0; i < times.size(); i++) {
-        // move ptr_finished
-        while(ptr_finished < i && output[ptr_finished] <= times[i]) {
-            // people leave immediately
-            if (output[ptr_finished] != times[ptr_finished])
-                queue_size--;
-            
-            ptr_finished++;
-        }
-        // check queue_size
-        if (queue_size >= num_max_waiting) { // 1
-            output[i] = times[i];
-            continue;
-        }
-        
-        cout << "ptr_finished: " << ptr_finished << '\n';
-        output[i] = max(last_finish_time + waiting_time, times[i] + waiting_time);
-        last_finish_time = max(last_finish_time, output[i]);
+      while (ptr_processing < i && times[i] >= finished_times[ptr_processing]) {
+        // udpate ptr_processing
+        if (times[ptr_processing] != finished_times[ptr_processing])
+          queue_size--;
+        ptr_processing++;
+      }
+
+      if (queue_size > num_max_waiting) {
+        finished_times[i] = times[i];
+        continue;
+      }
+      
+      // update variables
+      if (times[i] < last_finish_time) {
+        last_finish_time = last_finish_time + waiting_time;
         queue_size++;
+      }
+      else {
+        last_finish_time = times[i] + waiting_time;
+      }
+      finished_times[i] = last_finish_time;
     }
-    return output;
+    return finished_times;
   }
 };
-/* 
-      time: [  4, 400, 450, 500]
-    output: [304, 700,1000,   0]
-               ^
+/*
+      time: [  4, 400, 450, 800]
+    output: [304, 700,1000, 800]
+                        ^
     steps:
-        1) move ptr_finished
+        1) move ptr_processing
         2) check queue_size
         3) update queue_size
 
     variables:
         last_finish_time
-        ptr_finished: 1
-        queue_size = 1
+        ptr_processing
+        queue_size
  */
 int main(int argc, char **argv) {
-
   vector<int> times{4, 400, 450, 500, 700};
   Solution sol;
   vector<int> ans = sol.output(times);
-  print (ans);
-  
+  print (times);
+  print(ans);
+
   times = {4, 400, 450, 500, 699};
   ans = sol.output(times);
-  print (ans);
-//   std::cout << "ans: " << ans << '\n';
+  print (times);
+  print(ans);
+  //   std::cout << "ans: " << ans << '\n';
 
   return 0;
 }
