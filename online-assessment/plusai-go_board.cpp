177546424d0acab9c26280c0f4dd1f50dfb49487
Jacky Yeh <jackyyeh511@gmail.com>
2024-12-04 20:38:01 -0600

    update go game

--- online-assessment/plusai-go_board.cpp
+++ online-assessment/plusai-go_board.cpp
@@ -1,86 +1,113 @@
 #include <iostream>
+#include <vector>
+#include <utility>
 using namespace std;
 
-// Given a go board
-// e.g.
-// 0 0 0 0 0 0
-// 0 0 1 0 0 0
-// 0 1 2 1 0 0
-// 0 1 2 1 0 0
-// 0 1 2 1 0 0
+bool dfs(vector<vector<int>> &board, int cur_row, int cur_col, vector<pair<int, int>> &completed, vector<vector<int>> &visited) {
+    int n_row = board.size();
+    int n_col = board[0].size();
 
-queue<pair<int, int>> {
-}
-
-vector<pair<int, int>> { // completed coord
-  {2,2},
-  {3,2},
-  {4, 2}
-}
-// 1 2 2
-// 2 2 2
-// 2 2 2
-
-// remove the surrounded 2s
-bool dfs(vector<vector<int>> &board, int cur_row, int cur_col, vector<pair<int, int>>& completed, vector<vector<bool>> &visited) {
-  
-  if (visited[cur_row][cur_col]) return true;
-  visited[cur_row][cur_col] = true;
-
-  int n_row = board.size();
-  int n_col = board[0].size();
-
-  // four directions
-  vector<vector<int>> dirs{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-  completed.push_back({cur_row, cur_col});
-
-  // check four directions
-  for (const auto & dir : dirs) {
-    int next_row = cur_row + dir[0];
-    int next_col = cur_col + dir[1];
-    
-    if (next_row < 0 || next_row >= n_row || next_col < 0 || next_col >= n_col)
-      continue;
-    if (board[next_row][next_col] == 0)
+    // If out of bounds or already visited, skip
+    if (visited[cur_row][cur_col] == 1) return true;
+    else if (visited[cur_row][cur_col] == 2) {
+      visited[cur_row][cur_col] = 2;
       return false;
-    // if (board[next_row][next_col] == 1)  // not need to do anything
-    
-    if (board[next_row][next_col] == 2) {
-      bool should_modified = dfs(board, next_row, next_col, completed, visited);
-      if (!should_modified)
-        return false;
     }
-  }
-  return true;
+
+    // Mark as visited
+    visited[cur_row][cur_col] = 1; // visiting
+
+    // Add current cell to the completed list
+    completed.emplace_back(cur_row, cur_col);
+
+    // Directions: right, down, left, up
+    vector<vector<int>> dirs{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    bool should_modified = true;
+    for (const auto &dir : dirs) {
+        int next_row = cur_row + dir[0];
+        int next_col = cur_col + dir[1];
+
+        // check boundary
+        if (next_row < 0 || next_row >= n_row || next_col < 0 || next_col >= n_col)
+          continue;
+        if (board[next_row][next_col] == 0) {
+          should_modified = false;
+        }
+        // else if (board[next_row][next_col] == 1) // do nothing
+        else if (board[next_row][next_col] == 2) {
+            if (!dfs(board, next_row, next_col, completed, visited)) {
+              should_modified = false;
+              break;
+            }
+        }
+    }
+    if (should_modified) return true;
+    
+    visited[cur_row][cur_col] = 2; // visited, not surrounded by 1
+    return false;
 }
 
 void goBoard(vector<vector<int>> &board) {
-  int n_row = board.size();
-  int n_col = board[0].size();
-  
-  vector<vector<bool>> visited(n_row, vector<bool>(n_col));
-  for (int r = 0; r < n_row; r++) {
-    for (int c = 0; c < n_col; c++) {
-      if (board[r][c] == 2) {
-        vector<pair<int, int>> completed;
-        bool should_modified = dfs(board, r, c, completed, visited);
-        if (should_modified) {
-          // TODO
-          // remove two to zero based on completed
-          for (const auto& [row, col] : completed) {
-            board[row][col] = 0;
-          }
+    int n_row = board.size();
+    int n_col = board[0].size();
+
+    // Track visited cells
+    vector<vector<int>> visited(n_row, vector<int>(n_col));
+
+    for (int r = 0; r < n_row; r++) {
+        for (int c = 0; c < n_col; c++) {
+            if (board[r][c] != 2 || visited[r][c] != 0) 
+              continue;
+            
+            // dfs
+            vector<pair<int, int>> completed;
+            if (dfs(board, r, c, completed, visited)) {
+                // Modify surrounded components
+                for (const auto &[row, col] : completed) {
+                    board[row][col] = 0;
+                }
+            }
         }
-      }
     }
-  }
+
+    // Print updated board
+    cout << "visited matrix:\n";
+    for (const auto &row : visited) {
+        for (int cell : row) {
+            cout << cell << " ";
+        }
+        cout << endl;
+    }
+
 }
 
-// To execute C++, please define "int main()"
+/* 
+  status only goes to coord with value 2.
+    status = 0 // not visited
+    status = 1 // visiting
+    status = 2 // visited, and not surrounding by 1
+    status = 3 // visited, and surrounding by 1 (not needed, because we modify 2 to zero every iteration)
+ */
 int main() {
-  auto words = { "Hello, ", "World!", "\n" };
-  for (const char* const& word : words) {
-    cout << word;
-  }
-  return 0;
+    // Example board
+    vector<vector<int>> board = {
+        {0, 0, 0, 0, 0, 0},
+        {0, 0, 1, 0, 0, 0},
+        {0, 1, 2, 1, 1, 0},
+        {0, 1, 2, 1, 2, 1},
+        {0, 1, 2, 2, 1, 0}
+    };
+
+    goBoard(board);
+
+    // Print updated board
+    cout << "\nfinal board:\n";
+    for (const auto &row : board) {
+        for (int cell : row) {
+            cout << cell << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
